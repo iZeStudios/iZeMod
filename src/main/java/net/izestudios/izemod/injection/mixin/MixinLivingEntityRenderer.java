9f9817af48ee02a9dae8621ef621aa6c6e970f53
Maurice <mau.topf@gmail.com>
2025-05-01 00:10:05 +0200

    Show name tag in third person for local player (#42)
    * added MixinLivingEntityRenderer
    
    * Check for camera entity, code style
    
    ---------
    
    Co-authored-by: FlorianMichael <florian.michael07@gmail.com>

---
+++ src/main/java/net/izestudios/izemod/injection/mixin/MixinLivingEntityRenderer.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of iZeMod - https://github.com/iZeStudios/iZeMod
+ * Copyright (C) 2025 iZeStudios and GitHub contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.izestudios.izemod.injection.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntityRenderer.class)
+public abstract class MixinLivingEntityRenderer {
+
+    @Inject(method = "shouldShowName(Lnet/minecraft/world/entity/LivingEntity;D)Z", at = @At("HEAD"), cancellable = true)
+    private void showNameTagInThirdPerson(LivingEntity entity, double distanceSquared, CallbackInfoReturnable<Boolean> cir) {
+        if (entity == Minecraft.getInstance().getCameraEntity()) {
+            cir.setReturnValue(Minecraft.renderNames());
+        }
+    }
+
+}
