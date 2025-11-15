3d6d4b2820cd1cb281e564250fc6fad769b9bf88
FlorianMichael <florian.michael07@gmail.com>
2025-11-15 21:01:11 +0100

    Update VFP include

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,11 @@ repositories {
     mavenCentral()
     maven("https://repo.viaversion.com")
     maven("https://maven.lenni0451.net/everything")
-    maven("https://repo.opencollab.dev/maven-snapshots/")
+    maven("https://repo.opencollab.dev/maven-snapshots")
     maven("https://maven.terraformersmc.com/releases")
     maven("https://jitpack.io") {
         content {
-            includeGroup("com.github.Oryxel")
+            includeGroup("com.github.oryxel1")
             includeGroup("com.github.iZeStudios")
         }
     }
@@ -36,6 +36,6 @@ val jij = configureJij()
 dependencies {
     jij("com.github.iZeStudios:discord-ipc:b0d38c7edc")
 
-    modImplementation("com.viaversion:viafabricplus-api:4.3.2")
-    modRuntimeOnly("com.viaversion:viafabricplus:4.3.2")
+    modImplementation("com.viaversion:viafabricplus-api:4.3.3")
+    modRuntimeOnly("com.viaversion:viafabricplus:4.3.3")
 }
