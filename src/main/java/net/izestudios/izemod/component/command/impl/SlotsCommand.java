c0da82ada5436fb9ff531498e76b6df8a46913a8
Maurice <mau.topf@gmail.com>
2025-12-22 02:23:52 +0100

    Add Slots Command

---
+++ src/main/java/net/izestudios/izemod/component/command/impl/SlotsCommand.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of iZeMod - https://github.com/iZeStudios/iZeMod
+ * Copyright (C) 2025 iZeStudios and GitHub contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.izestudios.izemod.component.command.impl;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.izestudios.izemod.api.command.AbstractCommand;
+import net.minecraft.client.Minecraft;
+import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.network.chat.Component;
+
+public class SlotsCommand extends AbstractCommand {
+
+    public SlotsCommand() {
+        super(Component.literal("commands.slots"), "slots");
+    }
+
+    @Override
+    public void builder(final LiteralArgumentBuilder<SharedSuggestionProvider> builder) {
+        builder.executes(commandContext -> {
+            if(Minecraft.getInstance().isLocalServer()){
+                printErrorMessage(Component.translatable("commands.slots.singleplayer"));
+                return FAILURE;
+            }
+
+            final int max = Minecraft.getInstance().getCurrentServer().players.max();
+            printSuccessMessage(Component.translatable("commands.slots.success", max));
+            return SUCCESS;
+        });
+    }
+
+}
